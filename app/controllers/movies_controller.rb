7b02d08e3e750190a576a74406beb9acf0e365a0
Kaleb Gwyn <gwynkaleb03@gmail.com>
2024-04-24 21:44:49 -0500

    created movie show page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,9 @@ class MoviesController < ApplicationController
         @movies = Movie.all
         render :index
     end
+
+    def show
+        @movie = Movie.find(params[:id])
+        render :show
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
   get "books/:id", to: "books#show", as: "book"
 
   get "movies", to: "movies#index", as: "movies"
+  get "movies/:id", to: "movies#show", as: "movie"
 end
